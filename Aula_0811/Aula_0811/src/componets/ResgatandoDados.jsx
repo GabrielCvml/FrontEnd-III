f6402c026b8e7978bdb12f8d78b51caf2d9d6f73
GabrielCvml <gabrielcavalcanteml14@gmail.com>
2023-11-11 23:09:54 -0300

    aula08

--- Aula_0811/Aula_0811/src/componets/ResgatandoDados.jsx
+++ Aula_0811/Aula_0811/src/componets/ResgatandoDados.jsx
@@ -1,23 +1,43 @@
-import { useState, useEffect } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react';
 
-const url = "http://localhost:3030/alunos"
+const url = "http://localhost:3000/alunos"
 
 const ResgatandoDados = () => {
-  const [alunos, setAlunos] = useState([])
+  const [alunos, setAlunos] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
 
-      const res = await fetch(url)
-      const data = await res.json()
-
-      setAlunos(data)
+      setAlunos(data);
     }
-    fetchData()
+    fetchData();
     },[])
   return (
     <>
-      <p></p>
+      <h5>Lista de alunos</h5>
+      <table>
+        <thead>
+          <tr>
+            <th>Matrícula</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Curso</th>
+          </tr>
+        </thead>
+        <tbody>
+          {alunos.map((a) => (
+            <tr>
+              <th style={{padding:10}} >{a.id}</th>
+              <th style={{padding:10}}>{a.nome}</th>
+              <th style={{padding:10}}>{a.email}</th>
+              <th style={{padding:10}}>{a.curso}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
